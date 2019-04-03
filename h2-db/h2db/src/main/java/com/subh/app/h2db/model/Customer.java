1d6ebea8e21e750bb3f8de8d3754de575700414f
Subhash Kumar Singh <subh.singh007@gmail.com>
2019-04-03 22:47:52 +0530

    add id and autogenerated annotation to entitiy

--- h2-db/h2db/src/main/java/com/subh/app/h2db/model/Customer.java
+++ h2-db/h2db/src/main/java/com/subh/app/h2db/model/Customer.java
@@ -8,14 +8,11 @@ import javax.persistence.Id;
 
 @Entity
 public class Customer {
-
-    @Id
-    @GeneratedValue        /// why generatedValue is here  ??
+    /// why generatedValue is here  ??
     public Long getId() {
         return id;
     }
 
-    
     public void setId(Long id) {
         this.id = id;
     }
@@ -36,6 +33,8 @@ public class Customer {
         this.address = address;
     }
 
+    @Id
+    @GeneratedValue
     Long id;
     String name;
     String address;
