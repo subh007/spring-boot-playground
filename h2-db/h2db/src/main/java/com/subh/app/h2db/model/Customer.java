6cd016298f0badfb02d8b97a477a6b32dbdf10f3
Subhash Kumar Singh <subh.singh007@gmail.com>
2019-04-04 00:35:45 +0530

    enable configuration with mysql db

--- h2-db/h2db/src/main/java/com/subh/app/h2db/model/Customer.java
+++ h2-db/h2db/src/main/java/com/subh/app/h2db/model/Customer.java
@@ -2,6 +2,7 @@ package com.subh.app.h2db.model;
 
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -35,7 +36,12 @@ public class Customer {
 
     @Id
     @GeneratedValue
+    @Column(name = "id")
     Long id;
+
+    @Column(name = "name")
     String name;
+
+    @Column(name = "address")
     String address;
 }
