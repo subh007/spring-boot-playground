988e71b3c88d85c8ab931d2bf5153a42f535375a
Subhash Kumar Singh <subh.singh007@gmail.com>
2019-04-16 00:01:56 +0530

    example for writing sample unit test.

--- h2-db/h2db/src/test/java/com/subh/app/h2db/H2dbApplicationTests.java
+++ h2-db/h2db/src/test/java/com/subh/app/h2db/H2dbApplicationTests.java
@@ -1,16 +1,56 @@
 package com.subh.app.h2db;
 
+import com.subh.app.h2db.repository.BillRepository;
+import com.subh.app.h2db.repository.CustomerRepository;
+import com.subh.app.h2db.service.FileStorageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class H2dbApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	CustomerRepository customerRepository;
+
+	@MockBean
+	FileStorageService fileStorageService;
+
+	@MockBean
+	BillRepository billRepository;
+
 	@Test
-	public void contextLoads() {
+	public void simpleUnitTest() throws Exception {
+
+		Mockito.when(customerRepository.findAll()).thenReturn(
+				Collections.emptyList());
+
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/customer/all")
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Basic YWRtaW46YWRtaW4=")).andReturn();
+
+		Mockito.verify(customerRepository).findAll();
 	}
+
 
 }
